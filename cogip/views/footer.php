f34aeaff1e3ff66673476226262405b03f301178
Mathieu Kruk <mathieukruk@gmail.com>
2019-10-28 15:37:00 +0100

    Adding basic .php files

---
+++ cogip/views/footer.php
@@ -0,0 +1,22 @@
+<html>
+    <div class="container-fluid">
+        <div class="row footer" >
+
+            <div class="col-md-6 col-12 rounded-lg d-flex justify-content-center">
+				<!--TripAdvisor-->
+				<!-- <a link href="https://fr.tripadvisor.be/" target="_blank"><i class="fa fa-tripadvisor" aria-hidden="true"></i></a> -->
+				<!--Facebook-->
+				<a link href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+				<!--Twitter-->
+				<a link href="https://twitter.com/" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+				<!--Instagram-->
+				<a link href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+            </div>
+            
+            <div class="col-md-6 col-12 footer copyright rounded-lg d-flex justify-content-center">
+                <small>&copy; Copyright 2019, Gajwak</small>
+            </div>
+
+        </div>
+    </div>
+</html>
