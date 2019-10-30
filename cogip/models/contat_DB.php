925eb07342d655cb7a7be67b7e4e9ea52ecaa43d
Zjinane <zekji@hotmail.com>
2019-10-30 17:02:19 +0100

    add function

--- cogip/models/contat_DB.php
+++ cogip/models/contat_DB.php
@@ -2,23 +2,73 @@
 
 //connection à la Base de données
 function connectionDB(){
-
+	//variable de connexion a la database
+	$serverName = "database";
+	$userName = "root";
+	$password ="root";	
+	$dbName = "cogip";
+	//connection a la database
+	$conn = mysqli_connect($serverName,$userName,$password,$dbName);
+	if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+	return $conn;
 };
 
 
 //creer un contact
-function createContac($firstname,$lastname,$email,$compagnie,$Invoices){
+function createContac($firstname,$lastname,$email,$type){
+	//insertion data dans la database
+	$sql = "INSERT INTO contacts (firstname,lastname,email,fk_companies) VALUES ('$firstname', '$lastname', '$email',$type)";
 
+	if (mysqli_query(connectionDB(), $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error(connectionDB());
+}
+	return $sql;
 };
 
 
+function readAllContact(){
+	$sql = "SELECT firstname, lastname, email  FROM contacts";
+	$result = mysqli_query(connectionDB(), $sql);
+
+	if (mysqli_num_rows($result) > 0) {
+		while($row = mysqli_fetch_assoc($result)) {
+			echo "NAME: " . $row["firstname"]. "  ".$row["lastname"]. " EMAIL : " . $row["email"]. "<br>";
+		}
+	} else {
+		echo "0 results";
+	}
+};
+
+// faut verifier ici :)
 //afficher un contact ( lire un contact)
-function readContact($id){
+function readContact($firstname,$lastname){
+	$sql = "SELECT firstname, lastname, email FROM contacts = '$firstname' , '$lastname' , '$email' ";
+	$result = mysqli_query(connectionDB(),$sql);
+	$array = mysqli_fetch_assoc($result);
+	return $array;
 
 };
-
-//mettre/modifier les infos du contact
-function upDateContact($firstname,$lastname,$email,$compagnie,$Invoices){
+readContact("lala","lolo");
+//mettre/modifier les infos du contact fonction a utilisé avec readContact();
+function upDateContact($firstname,$lastname,$email,$compagnies){
+	//recupere l'id dans l'url
+	$id = $_GET['id'];
+	$sql = "SELECT * FROM contacts WHERE id ='". $id ."' limit 1";
+	$result = mysqli_query(connectionDB(),$sql);
+	$resulta = mysqli_num_rows($result);
+    $row = mysqli_fetch_assoc($result);
+	//donner au parametre une valeur 
+	$lastname = $row["lastname"] ;
+	$firstname = $row["firstname"] ; 
+	$email = $row["email"];
+	// recupere  fk_companies name
+	$fk = $_GET["fk_companies"];
+	$sqll = "SELECT name FROM companies WHERE fk_companies = '" . $fk ."' limit 1"; 
+	$compagnies = mysqli_query(connectionDB(),$sqll);
 
 };
 
@@ -27,5 +77,9 @@ function upDateContact($firstname,$lastname,$email,$compagnie,$Invoices){
 function deleteContact($id){
 
 };
+
+
+
+readContact("lala", "lolo");
 
 ?>
