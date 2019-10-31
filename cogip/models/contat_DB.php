4abcdbfaa1950cb8e1f84cfa35b6705b22564ddb
Shaunyx <e_jeremy@outlook.com>
2019-10-31 16:28:17 +0100

    Function clean et review avec ajout d'un message texte pour jinane du taf effectué

--- cogip/models/contat_DB.php
+++ cogip/models/contat_DB.php
@@ -1,85 +1,136 @@
 <?php
 
-//connection à la Base de données
-function connectionDB(){
-	//variable de connexion a la database
-	$serverName = "database";
-	$userName = "root";
-	$password ="root";	
-	$dbName = "cogip";
-	//connection a la database
-	$conn = mysqli_connect($serverName,$userName,$password,$dbName);
-	if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-	return $conn;
+		//connection à la Base de données
+		
+		function connectionDB(){
+		
+		//variable de connexion a la database
+	
+		$serverName = "database";
+		$userName = "root";
+		$password ="root";	
+		$dbName = "cogip";
+		
+		//connection a la database
+		$conn = mysqli_connect($serverName,$userName,$password,$dbName);
+		if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+			}
+		return $conn;
 };
+		// Fonction de menu déroulant pour le choix de pays dans la page contact
+		
+		$sql= "SELECT name FROM compagnies";
+		array = ["$sql"];
+		foreach
 
+//#################################################################################
 
-//creer un contact
-function createContac($firstname,$lastname,$email,$type){
-	//insertion data dans la database
-	$sql = "INSERT INTO contacts (firstname,lastname,email,fk_companies) VALUES ('$firstname', '$lastname', '$email',$type)";
+		//creer un contact
 
-	if (mysqli_query(connectionDB(), $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error(connectionDB());
-}
+		function createContac($firstname,$lastname,$email,$type){
+	
+		//insertion data dans la database
+
+		$sql = "INSERT INTO contacts (firstname,lastname,email,fk_companies) VALUES ('$firstname', '$lastname', '$email',$type)";
+		if (mysqli_query(connectionDB(), $sql)) {
+			echo "New record created successfully";
+		}else{
+			echo "Error: " . $sql . "<br>" . mysqli_error(connectionDB());
+		}
 	return $sql;
 };
 
+//#################################################################################
 
-function readAllContact(){
-	$sql = "SELECT firstname, lastname, email  FROM contacts";
-	$result = mysqli_query(connectionDB(), $sql);
+		//permets de lire tous les contacts et de les afficher
+		function readAllContact(){
+		$sql = "SELECT firstname, lastname, email  FROM contacts";
+		$result = mysqli_query(connectionDB(), $sql);
 
-	if (mysqli_num_rows($result) > 0) {
-		while($row = mysqli_fetch_assoc($result)) {
-			echo "NAME: " . $row["firstname"]. "  ".$row["lastname"]. " EMAIL : " . $row["email"]. "<br>";
+		if (mysqli_num_rows($result) > 0) {
+			while($row = mysqli_fetch_assoc($result)) {
+				echo "NAME: " . $row["firstname"]. "  ".$row["lastname"]. " EMAIL : " . $row["email"]. "<br>";
+				}
+		}else{
+			echo "0 results";
 		}
-	} else {
-		echo "0 results";
-	}
 };
 
-// faut verifier ici :)
-//afficher un contact ( lire un contact)
-function readContact($firstname,$lastname){
-	$sql = "SELECT firstname, lastname, email FROM contacts = '$firstname' , '$lastname' , '$email' ";
-	$result = mysqli_query(connectionDB(),$sql);
-	$array = mysqli_fetch_assoc($result);
-	return $array;
 
-};
-readContact("lala","lolo");
+//#################################################################################
+
+
+///###///###///###///###///###///###///###///###///###///###_! ICI Il faut check !_###///###///###///###///###///###///###///###///###///###///
+
+		//afficher un contact ( lire un contact) dans un tableau et permettant via un lien d'acceder au infos en détails mais aussi au Tools (Update/Delete ou juste read les infos)
+
+		function ListContact($firstname,$lastname,$email){
+
+		// ici on selectione la partie qui va etre traité pour là mettre dans la fonction suivante
+
+		$sql= "SELECT * FROM contacts";
+		$result= mysqli_query(connectionDB(),$sql) or die(mysqli_error(connectionDB()));
+		$row = mysqli_fetch_assoc($result)
+		
+		// ici on crée la boucle;
+		//verifier la
+				foreach($row as $element){
+	//ici on insert les données de la DB dans le tableau de mathieu, pour chaque colonne à chaque row on y insert les données
+					echo "<tr><td>". $firstname . "</td><td>" . $lastname . "</td><td>" . $email "</td></tr>";
+
+				//--> voir le process pour traitez les infos avant de les renvoyées dans le tableau.
+				//--> ici ce sont les liens a add plus tard quii renveront au formulaire de modification comme, just voir ["view"]
+				//    mettre à jour ["Update"] et supprimer ["Delete"].
+
+					<td> <a href= "contact.php?id="/***/" & action= view"</a></td>
+					<td> <a href= "contact.php?id="/***/" & action= Update"</a></td>
+					<td> <a href= "contact.php?id="/***/" & action = Delete"</a><td>
+
+				})
+			};
+			//--> rappel la fonction qui renvera les donnés dans le tableau "contact" avec les liens evoqué plus haut.
+				
+		function ListContact();
+
+//#################################################################################
+
+
+
 //mettre/modifier les infos du contact fonction a utilisé avec readContact();
-function upDateContact($firstname,$lastname,$email,$compagnies){
-	//recupere l'id dans l'url
-	$id = $_GET['id'];
-	$sql = "SELECT * FROM contacts WHERE id ='". $id ."' limit 1";
-	$result = mysqli_query(connectionDB(),$sql);
-	$resulta = mysqli_num_rows($result);
-    $row = mysqli_fetch_assoc($result);
-	//donner au parametre une valeur 
-	$lastname = $row["lastname"] ;
-	$firstname = $row["firstname"] ; 
-	$email = $row["email"];
-	// recupere  fk_companies name
-	$fk = $_GET["fk_companies"];
-	$sqll = "SELECT name FROM companies WHERE fk_companies = '" . $fk ."' limit 1"; 
-	$compagnies = mysqli_query(connectionDB(),$sqll);
+
+		function upDateContact($firstname,$lastname,$email,$compagnies){
+
+		//recupere l'id dans l'url
+
+		$id = $_GET['id'];
+		$sql = "SELECT * FROM contacts WHERE id ='". $id ."' limit 1";
+		$result = mysqli_query(connectionDB(),$sql);
+		$resulta = mysqli_num_rows($result);
+		$row = mysqli_fetch_assoc($result);
+	
+		//donner au parametre une valeur
+		
+		$lastname = $row["lastname"] ;
+		$firstname = $row["firstname"] ; 
+		$email = $row["email"];
+	
+		// recupere les valeurs des colonnes -->  Name,  du tableau --> fk_companies
+
+		$fk = $_GET["fk_companies"];
+		$sqll = "SELECT name FROM companies WHERE fk_companies = '" . $fk ."' limit 1"; 
+		$compagnies = mysqli_query(connectionDB(),$sqll);
+};
+
+//#################################################################################
+
+		//supprimer une instance ( supprimer une row de la DB)
+		function deleteContact($id){
+
 
 };
 
 
-//supprimer une instance ( supprimer une row de la DB)
-function deleteContact($id){
 
-};
-
-
-
-readContact("lala", "lolo");
 
 ?>
--- cogip/models/database.php
+++ cogip/models/database.php
@@ -3,12 +3,34 @@
 //connection à la Base de données
 function connectionDB(){
 
+// var de connection necéssaire à la db
+	$serverName = "database";
+	$userName = "root";
+	$password = "root";
+	$dbName = "cogip";
+
+// connection à la DB
+	$conn= mysqli_connect($servername,$username,$password,$dbName);
+	return $conn;
+
+
+//verification de la connection de la Db au php files.
+
+//	if (!$conn){
+//		die("connection impossible: " . mysqli_connect_errno());
+//	}
+//	echo "connection Réussie";
+
 };
 
 
 //creer un utilisateur
-function createUser($userName,$password){
+function createUser($userName,$password,$usertype){
 
+	// Ajouter dans la DB un utilisateur.
+	$sql = INSERT INTO login (name,password,usertype)
+	VALUE ($username, $password, $usertype)
+	return $sql;
 };
 
 
@@ -18,7 +40,7 @@ function readUser($id){
 };
 
 //mettre/modifier les infos de l'utilisateur
-function upDate($username, $password){
+function upDate($username, $password,$usertype){
 
 };
 
