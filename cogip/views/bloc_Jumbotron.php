f1685cdae1507cd9b7df48289bb03ed32baac96c
Zjinane <zekji@hotmail.com>
2019-11-20 16:01:26 +0100

    git ignior

--- cogip/views/bloc_Jumbotron.php
+++
@@ -1,8 +0,0 @@
-<div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <div class="d-flex justify-content-left">
-        <h1>◽️ Cogip ◽️</h1>
-    </div>
-    <p class="lead">Better colors, quicker service and extrem fiability,<br> this is why cogip is your favorite compagny</p>
-  </div>
-</div>
